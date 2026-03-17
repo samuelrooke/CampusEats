cbaecf205d3375b7c9a6b489c0d1c725d6120f56
Samuel <srooke50@gmail.com>
2026-03-17 14:18:39 +0200

    feat(database): add database skeleton

---
+++ backend/src/database/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const query = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, params, (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
+};
+
+module.exports = { query };
